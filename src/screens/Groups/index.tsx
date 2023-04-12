404f840b7c07be98668bb89bc767d57b29a62f8a
allankruger <allanfloreskruger@gmail.com>
2023-04-12 15:58:41 -0300

    imports fonts from Google Fonts

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,6 @@
 import { View, Text } from "react-native";
-import { Container, Title } from "./styles";
+import { Container } from "./styles";
 
 export default function Groups() {
-  return (
-    <Container>
-      <Title>Groups</Title>
-    </Container>
-  );
+  return <Container></Container>;
 }
