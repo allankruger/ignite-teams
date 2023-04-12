6e743b094b24a656857f7268dba32d2af0b1d72e
allankruger <allanfloreskruger@gmail.com>
2023-04-12 16:17:28 -0300

    changes loading spinner color

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,11 @@
+import { Container, LoadingIndicator } from "./styles";
+
+import { View, Text } from "react-native";
+
+export default function Loading() {
+  return (
+    <Container>
+      <LoadingIndicator />
+    </Container>
+  );
+}
