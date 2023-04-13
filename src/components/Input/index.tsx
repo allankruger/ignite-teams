9802dde4ea451b674c35504821f31bfbd08c67f1
allankruger <allanfloreskruger@gmail.com>
2023-04-13 14:10:38 -0300

    creates Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,10 @@
+import { TextInputProps } from "react-native";
+import { Container } from "./styles";
+import { useTheme } from "styled-components/native";
+
+export function Input(props: TextInputProps) {
+  const { ...rest } = props;
+  const { COLORS } = useTheme();
+
+  return <Container placeholderTextColor={COLORS.GRAY_300} {...rest} />;
+}
--- src/screens/NewTeam/index.tsx
+++ src/screens/NewTeam/index.tsx
@@ -2,6 +2,7 @@ import { Container, Content, Icon } from "@screens/NewTeam/styles";
 import Header from "@components/Header";
 import PageInfo from "@components/PageInfo";
 import Button from "@components/Button";
+import { Input } from "@components/Input";
 
 export default function NewTeam() {
   return (
@@ -13,6 +14,7 @@ export default function NewTeam() {
           title="New team"
           subtitle="Create a new and start adding people"
         />
+        <Input placeholder="Team name" />
         <Button title="Create" />
       </Content>
     </Container>
